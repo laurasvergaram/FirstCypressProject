6fea128b245aacad91fbfebed162bb64678b575f
Laura Vergara <Laura.Vergara@endava.com>
2022-12-02 10:59:15 -0500

    Second Class

--- cypress/e2e/first_test.cy.js
+++ cypress/e2e/first_test.cy.js
@@ -1,5 +1,11 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('https://www.demoblaze.com/')
+    cy.get('#login2').click()
+    cy.get('#loginusername').type('user232')
+    cy.get('#loginpassword').type('password')
+    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+    cy.get('#nameofuser').should('have.text','Welcome user232')
+
   })
 })
