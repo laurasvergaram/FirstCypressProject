55d86fda405f7d3aacfd4df90b325e3ced3c4f11
Laura Vergara <Laura.Vergara@endava.com>
2022-12-07 11:58:01 -0500

    Third class working ASSERTIONS

--- cypress/e2e/first_test.cy.js
+++ cypress/e2e/first_test.cy.js
@@ -5,7 +5,33 @@ describe('empty spec', () => {
     cy.get('#loginusername').type('user232')
     cy.get('#loginpassword').type('password')
     cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
-    cy.get('#nameofuser').should('have.text','Welcome user232')
+    // cy.get('#nameofuser').should('have.text','Welcome user232')
+
+    // TIENE 4 ELEMENTOS EN ESE GRUPO
+    // cy.get(':nth-child(1) > .card > .card-block > h5').should('have.text','$360')
+    // cy.get('.list-group a').should('have.length',4)
+
+    // CAMBIA LA URL AL CAMBIAR DE VENTANA
+    // cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch').click()
+    // cy.url().should('equal','https://www.demoblaze.com/prod.html?idp_=1')
+
+    // CAMBIA LA URL AL CAMBIAR DE VENTANA FUNCION FLECHA
+    // cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch').click()
+    // cy.url().then((url)=>{expect(url).equal('https://www.demoblaze.com/prod.html?idp_=1')}) 
+    
+    // TDD VALIDAR SI EL TEXTO ES IGUAL
+    // cy.get('.name').then((element)=>{
+    //   expect(element).to.have.text('Samsung galaxy s6')
+    //   expect(element).to.have.length(1)
+    //  })
+    // cy.get('#nameofuser').should((nameofuser)=>{
+    //   expect(nameofuser).to.have.text('Welcome user232')
+    // })
+
+    // EJEMPLO CONTAINS
+    // cy.get('#logout2').contains('Log')
+    
+    
 
   })
 })
